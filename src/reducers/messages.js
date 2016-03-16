b9de1301f3c8b0e25ed52688b376ae30e49b07d2
jimenglish81 <jimenglish81@gmail.com>
2016-03-16 21:08:22 +0000

    room bug

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -3,7 +3,7 @@ const INITIAL_STATE = [];
 
 export default function(state=INITIAL_STATE, action) {
   switch (action.type) {
-    case ENTER_ROOM:
+    case `${ENTER_ROOM}_PENDING`:
     case `${LOGOUT}_FULFILLED`:
       return INITIAL_STATE;
     case MESSAGE_ADDED:
